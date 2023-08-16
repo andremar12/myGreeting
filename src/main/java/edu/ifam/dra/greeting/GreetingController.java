ff33e99ddf1e927f7931ecec6195ddc5d065c135
andremar12 <andremar.video15@gmail.com>
2023-08-16 18:08:18 -0400

    feat: metodo greetings

--- src/main/java/edu/ifam/dra/greeting/GreetingController.java
+++ src/main/java/edu/ifam/dra/greeting/GreetingController.java
@@ -1,5 +1,6 @@
 package edu.ifam.dra.greeting;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class GreetingController {
+	private ArrayList<Greeting> greetings = new ArrayList<Greeting>();
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		
+		greetings.add(new Greeting(counter.incrementAndGet(), String.format(template, name)));
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@GetMapping("/greetings")
+	public ArrayList<Greeting> greetings(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		return greetings;
 	}
 	
 }
